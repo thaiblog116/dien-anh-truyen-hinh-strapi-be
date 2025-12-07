e57bc4287bcc4657f58bf8a009286ab34b92c596
Nguyen Van Thai <thaiblog116@gmail.com>
2025-12-07 14:41:43 +0700

    thêm ảnh cho categories

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -615,6 +615,7 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
   };
   attributes: {
     articles: Schema.Attribute.Relation<'oneToMany', 'api::article.article'>;
+    cover: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
